79e2dca0f50d14b444ca346b01f4e814dfbc1fee
Jordan Mavrogeorge <jordomav@gmail.com>
2018-08-16 01:12:13 -0700

    switches to meteors accounts module

--- server/main.js
+++ server/main.js
@@ -1,12 +1,11 @@
 import { Meteor } from 'meteor/meteor';
-import '../imports/api/users';
-import { Users } from '../imports/api/users';
+import { Accounts } from 'meteor/accounts-base'
 
 Meteor.startup(() => {
   // code to run on server at startup
     Meteor.methods({
        'user.post' (user) {
-           Users.insert(user);
+           Accounts.createUser(user);
        }
     });
 });
